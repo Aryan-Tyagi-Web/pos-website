5916f264117e9fcb4377bc46607a4f8f8d85caf7
Aryan Tyagi <tyagiaryannda@gmail.com>
2026-08-15 10:49:28 +0530

    host

--- src/main.jsx
+++ src/main.jsx
@@ -10,14 +10,9 @@ import Solutions from './pages/Solutions.jsx'
 import Industries from './pages/Industries.jsx'
 import Company from './pages/Company.jsx'
 
-const isGitHubPages =
-  window.location.hostname.endsWith('github.io')
-
-const basename = isGitHubPages ? '/pos-website' : '/'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Routes>
 
         <Route path="/" element={<App />} />
